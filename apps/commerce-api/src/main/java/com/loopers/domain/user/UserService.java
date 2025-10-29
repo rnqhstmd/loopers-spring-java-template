b1b5d8081634480a79d93074f20c8037b6c7b494
BonSeung <rnqhstmd9134@naver.com>
2025-10-29 21:37:03 +0900

    feat: 유저 조회 메서드 추가

--- apps/commerce-api/src/main/java/com/loopers/domain/user/UserService.java
+++ apps/commerce-api/src/main/java/com/loopers/domain/user/UserService.java
@@ -10,15 +10,21 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class UserService {
 
-	private final UserRepository userRepository;
+    private final UserRepository userRepository;
 
-	@Transactional
-	public User signUp(String userId, String email, String birthDate, Gender gender) {
-		if (userRepository.existsByUserId(userId)) {
-			throw new CoreException(ErrorType.CONFLICT, "이미 가입된 ID입니다.");
-		}
+    @Transactional
+    public User signUp(String userId, String email, String birthDate, Gender gender) {
+        if (userRepository.existsByUserId(userId)) {
+            throw new CoreException(ErrorType.CONFLICT, "이미 가입된 ID입니다.");
+        }
 
-		User user = User.create(userId, email, birthDate, gender);
-		return userRepository.save(user);
-	}
+        User user = User.create(userId, email, birthDate, gender);
+        return userRepository.save(user);
+    }
+
+    @Transactional(readOnly = true)
+    public User getUserByUserId(String userId) {
+        return userRepository.findByUserId(userId)
+                .orElseThrow(() -> new CoreException(ErrorType.NOT_FOUND, "사용자를 찾을 수 없습니다."));
+    }
 }
