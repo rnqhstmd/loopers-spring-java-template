c947c5dbb4e16b806131951939034430409805ce
BonSeung <rnqhstmd9134@naver.com>
2025-10-29 20:44:07 +0900

    feat: UserFacde 클래스 추가 및 회원가입 추가

---
+++ apps/commerce-api/src/main/java/com/loopers/application/user/UserFacade.java
@@ -0,0 +1,20 @@
+package com.loopers.application.user;
+
+
+import com.loopers.domain.user.Gender;
+import com.loopers.domain.user.User;
+import com.loopers.domain.user.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class UserFacade {
+
+	private final UserService userService;
+
+    public UserInfo signUp(String userId, String email, String birthDate, Gender gender) {
+        User user = userService.signUp(userId, email, birthDate, gender);
+        return UserInfo.from(user);
+    }
+}
