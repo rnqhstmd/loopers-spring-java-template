126875ebeb523e66c90ee48623fa5de8a79af16b
BonSeung <rnqhstmd9134@naver.com>
2025-10-29 21:37:45 +0900

    feat: 유저 조회 API 메서드 추가

--- apps/commerce-api/src/main/java/com/loopers/interfaces/api/user/UserV1Controller.java
+++ apps/commerce-api/src/main/java/com/loopers/interfaces/api/user/UserV1Controller.java
@@ -5,10 +5,7 @@ import com.loopers.application.user.UserInfo;
 import com.loopers.interfaces.api.ApiResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
@@ -30,5 +27,16 @@ public class UserV1Controller implements UserV1ApiSpec {
                 request.gender()
         );
         return ApiResponse.success(UserV1Dto.UserResponse.from(userInfo));
+    }
+
+    @GetMapping("/me")
+    @Override
+    public ApiResponse<UserV1Dto.UserResponse> getMyInfo(
+            @RequestHeader("X-USER-ID") String userId
+    ) {
+        UserInfo userInfo = userFacade.getUserInfo(userId);
+        UserV1Dto.UserResponse response = UserV1Dto.UserResponse.from(userInfo);
+
+        return ApiResponse.success(response);
     }
 }
