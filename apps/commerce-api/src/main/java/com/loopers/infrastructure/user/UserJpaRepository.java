5b17cc93e8a2cbba6681b226bfa32d3f4ba59208
BonSeung <rnqhstmd9134@naver.com>
2025-10-30 15:02:58 +0900

    chore: 정렬 수정

--- apps/commerce-api/src/main/java/com/loopers/infrastructure/user/UserJpaRepository.java
+++ apps/commerce-api/src/main/java/com/loopers/infrastructure/user/UserJpaRepository.java
@@ -6,8 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 interface UserJpaRepository extends JpaRepository<User, Long> {
-
     Optional<User> findByUserId(String userId);
-
     boolean existsByUserId(String userId);
 }
